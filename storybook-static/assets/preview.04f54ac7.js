ab379e2dca24e2253320050d7afb9a85c9aa04ab
Pedro Tavolaro <phtc-pedro@hotmail.com>
2022-10-13 17:29:22 -0300

    first commit

---
+++ storybook-static/assets/preview.04f54ac7.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.4fcd3827.js";import"./iframe.2738bb45.js";import"./es.map.constructor.a2792393.js";import"./es.number.to-fixed.c613bf44.js";import"./index.8d1125a9.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.04f54ac7.js.map
